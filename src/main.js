5f9033fc7f96234424c24761d878535c18440f80
Erika Huang <suiic@qq.com>
2023-08-30 21:46:00 +0800

    feat: 项目初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router' // 导入router
+import ElementPlus from 'element-plus' // 导入elmentplus
+import 'element-plus/dist/index.css'
+
+console.log("环境变量=>", import.meta.env)
+
+const app = createApp(App) // 因为后续还需要导入其他东西，为了方便，这边把creatApp提出来了。
+
+app.use(router).mount('#app')
+app.use(ElementPlus)
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  server: {
+    host:'localhost', // 路径
+    port:8081, // 端口
+    open:true // 自动打开浏览器
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
